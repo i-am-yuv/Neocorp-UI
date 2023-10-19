d1766760f0d8332c0d6e601e471f1320f318ae7c
Gyana Ranjan Rout <gyanaranjan.r@splenta.com>
2023-10-19 09:16:04 +0530

    cash memo and receipt note

--- src/app/profile/profile-models.ts
+++ src/app/profile/profile-models.ts
@@ -7,52 +7,6 @@ import { ProductCategory } from "./product-category";
 export interface ProfileModels {
 }
 export interface Product {
-<<<<<<< Updated upstream
-    id?: string;
-    name?: string;
-    model?: string;
-    searchKey ?: string;
-    description ?: string,
-    skuCode?: string,
-    barCode?: string,
-    thumbnail?: string,
-    help ?: string
-    hsnCode?: string,
-    imageName?: string
-    imagePath?: string
-    productType ?: string,
-    category ?:Product ,
-    taxRate ?: TaxRate ,
-    brand ?: Brand ,
-    uom ?: UOM,
-    mrp ?: number 
-  }
-
-  enum ProductType {
-    Type1 = 'ITEM',
-    Type2 = 'SERVICE'
-  }
-
-  export interface Beneficiary{
-    id ?: string,
-    beneficaryName ?: string,
-    nickname ?: string,
-    accountNumber ?: string,
-    ifscCode ?: string,
-    mobileNumber ?: number,
-    mmid ?: string,
-    signupTime ?: any;
-    
-  }
-
-  export interface State {
-    id?: string;
-    stateName?: string;
-    stateCode?: string;
-    country ?: any ;
-    isTaxZone ?: boolean ;
-    isUT ?: boolean ;
-=======
   id?: string;
   name?: string;
   model?: string;
@@ -98,5 +52,4 @@ export interface State {
   country?: any;
   isTaxZone?: boolean;
   isUT?: boolean;
->>>>>>> Stashed changes
 }
