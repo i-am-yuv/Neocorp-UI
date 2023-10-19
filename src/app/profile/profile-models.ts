1e9236e6ae4d92100d7ed7979a891dfbe2f21ce0
Gyana Ranjan Rout <gyanaranjan.r@splenta.com>
2023-10-19 09:16:58 +0530

    cash memo and receipt note

--- src/app/profile/profile-models.ts
+++ src/app/profile/profile-models.ts
@@ -7,49 +7,48 @@ import { ProductCategory } from "./product-category";
 export interface ProfileModels {
 }
 export interface Product {
-  id?: string;
-  name?: string;
-  model?: string;
-  searchKey?: string;
-  description?: string,
-  skuCode?: string,
-  barCode?: string,
-  thumbnail?: string,
-  help?: string
-  hsnCode?: string,
-  imageName?: string
-  imagePath?: string
-  productType?: string,
-  category?: ProductCategories,
-  taxRate?: TaxRate,
-  brand?: Brand,
-  uom?: UOM,
-  mrp?: number
-}
+    id?: string;
+    name?: string;
+    model?: string;
+    searchKey ?: string;
+    description ?: string,
+    skuCode?: string,
+    barCode?: string,
+    thumbnail?: string,
+    help ?: string
+    hsnCode?: string,
+    imageName?: string
+    imagePath?: string
+    productType ?: string,
+    category ?:Product ,
+    taxRate ?: TaxRate ,
+    brand ?: Brand ,
+    uom ?: UOM,
+    mrp ?: number 
+  }
 
-enum ProductType {
-  Type1 = 'ITEM',
-  Type2 = 'SERVICE'
-}
+  enum ProductType {
+    Type1 = 'ITEM',
+    Type2 = 'SERVICE'
+  }
 
-export interface Beneficiary {
-  id?: string,
-  beneficaryName?: string,
-  nickname?: string,
-  accountNumber?: string,
-  ifscCode?: string,
-  mobileNumber?: number,
-  mmid?: string,
-  signupTime?: any;
-  coolingPeriodEnd?: any;
-  inCoolingPeriod?: boolean
-}
+  export interface Beneficiary{
+    id ?: string,
+    beneficaryName ?: string,
+    nickname ?: string,
+    accountNumber ?: string,
+    ifscCode ?: string,
+    mobileNumber ?: number,
+    mmid ?: string,
+    signupTime ?: any;
+    inCoolingPeriod?: boolean;
+  }
 
-export interface State {
-  id?: string;
-  stateName?: string;
-  stateCode?: string;
-  country?: any;
-  isTaxZone?: boolean;
-  isUT?: boolean;
+  export interface State {
+    id?: string;
+    stateName?: string;
+    stateCode?: string;
+    country ?: any ;
+    isTaxZone ?: boolean ;
+    isUT ?: boolean ;
 }
