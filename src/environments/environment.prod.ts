814eac89a919ea48b4baf09e2ac7b67ab48dc3c7
yuvraj <yuvraj.b@splenta.com>
2023-11-10 09:36:13 +0530

    added neocorp in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //apiurl: 'https://api.neomaxer.com/api',
-  apiurl: 'https://api.ifyn.com/api',
+  apiurl: 'https://api.ifyn.com/neocorp/api',
   apiurlNew: 'https://api.ifyn.com/neocorp/', 
 
   docsurl: 'https://portal.neomaxer.com/docs/info',
